e1932204f47b5d67fe5b063595e809c22cebd8b5
Dan Büschlen <dab@feinheit.ch>
2020-08-17 13:48:45 +0200

    Updates

---
+++ _next/static/rSYZIuxVwJ3CoL6kOP7Z8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/posts/[id]":[a,b]}}("static\u002Fchunks\u002Fd87d93cc0eb20dc671c48956578599c2f171e7e2.b1664d64bc319c7ac1cd.js","static\u002Fcss\u002F2ccd803ce29478a7b2ab.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
